49ebc7eaa89e1ed132d46b2aa70be9bcc802fc65
Markus Krimmel <montcyril@gmail.com>
2019-03-03 19:50:16 +0100

    Added functions as boundaries in quad

--- analysis/quad.py
+++ analysis/quad.py
@@ -22,23 +22,60 @@ def simpson(f, a, b, N):
     c = 8*(h/6)*np.sum(fx[1::2])
     return a+b+c
 
-def cross(f, y):
+
+def sliced(f, y):
     def g(*args):
         return f(*args, y)
 
     return g
 
-    
+def sliced_list(lf, y):
+    return [sliced(f, y) for f in lf]
+
 def quad_ruleNd(rule, n):
+    """
+    Generates a high dimensional quad rule from 1-dimensional blue-print
+
+    Arguments:
+    
+    rule: A quadrature rule which expects:
+    
+        - g: a function expecting 1 argument, returning a float
+        - a: float, lower bounds
+        - b: float, upper bounds
+        - N: resolution
+
+    n:    Dimensions, a positive integer
+
+    Returns a quadrature rule which expects:
+
+        - f: A function with n float arguments, returns a float
+
+        - bounds: A list of n functions, which return a 2-tuple of floats. The first function should expect n-1 arguments, the second n-2 etc.
+
+        - nodes: A list of positive integers, representing the resolution along each axis
+
+    Equivalent to:
+
+    nquad(f, [lambda a1, a2, ... an: bounds[0](an, ... a1), ....])
+    """
+    
     if(n==1):
-        return lambda f, bounds, nodes: rule(f, bounds[0][0], bounds[0][1], nodes[0])
+        return lambda f, bounds, nodes: rule(f, bounds[0]()[0], bounds[0]()[1], nodes[0])
+
+    lower_rule = quad_ruleNd(rule, n-1)
     
     def f(f, bounds, nodes):
-        lower_rule = quad_ruleNd(rule, n-1)
-        integral = lambda y:  lower_rule(cross(f, y), bounds[:-1], nodes[:-1])
-        return rule(np.vectorize(integral), bounds[-1][0], bounds[-1][1], nodes[-1])
+        integral = lambda y:  lower_rule(sliced(f, y), sliced_list(bounds[:-1], y), nodes[:-1])
+        return rule(np.vectorize(integral), bounds[-1]()[0], bounds[-1]()[1], nodes[-1])
 
     return f
+
+
+def cube_quad_ruleNd(rule, n):
+    qnd = quad_ruleNd(rule, n)
+    return lambda f, bounds, nodes: qnd(f, [lambda *args: bound for bound in bounds], nodes)
+
 
 
 if __name__ == "__main__":
