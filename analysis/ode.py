15b9f2014da4af5dead41bc76110e1ed5f0f5edf
Markus Krimmel <montcyril@gmail.com>
2019-03-14 20:34:01 +0100

    Removed boilerplate code

--- analysis/ode.py
+++ analysis/ode.py
@@ -4,34 +4,31 @@ import numpy as np
 
 w = 3
 
-def explicit_euler(f, y0, T, N):
+def _solve_ode(f, y0, N):
     ls = np.zeros((N, *y0.shape))
     ls[0] = y0
-    h = T/N
-    x = 0
-    
+        
     for i in range(1,N):
-        ls[i] = ls[i-1]+h*f(ls[i-1], x)
-        x+=h
-
+        ls[i] = f(ls[i-1], i-1)
+            
     return ls
 
 
-def implicit_euler(f, y0, T, N):
-    ls = np.zeros((N, *y0.shape))
-    ls[0] = y0
-    h = T/N
-    x = 0
-    for i in range(1,N):
-        ls[i] = fsolve(lambda y: y-ls[i-1]-h*f(y, x), ls[i-1])
-        x+=h
+def explicit_euler(rhs, y0, T, N):
+    h = T/(N-1)
+    return _solve_ode(lambda y_previous, i: y_previous+h*rhs(y_previous, i*h), y0, N)                                   //TODO: Check
 
-    return ls
+
+def implicit_euler(rhs, y0, T, N):
+    h = T/(N-1)
+    return _solve_ode(lambda y_previous, i: fsolve(lambda y: y-y_previous-h*rhs(y, (i+1)*h), y_previous), y0, N)        //TODO: Check
+
+
 
 
 def f(x, t):
     return np.array([x[1], -w**2*np.sin(x[0])])
 
 if __name__=="__main__":
-    plt.plot(explicit_euler(f, np.array([0.5, 0]), 6, 2500))
+    plt.plot(implicit_euler(f, np.array([0.5,0]), 3, 2000))
     plt.show()
