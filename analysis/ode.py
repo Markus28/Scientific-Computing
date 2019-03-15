12a2893b8406197e73a2833bec6da3c2f75a9d01
Markus Krimmel <montcyril@gmail.com>
2019-03-15 09:38:50 +0100

    Added implicit midpoint/trapezoidal rule

--- analysis/ode.py
+++ analysis/ode.py
@@ -1,17 +1,25 @@
 import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 import numpy as np
+import timeit
 
-w = 3
+gr = 9.81
+l = 1
+m=1
+w = (gr/l)**0.5
 
 def _solve_ode(f, y0, N):
-    ls = np.zeros((N, *y0.shape))
+    if type(y0)==np.ndarray:
+        ls = np.zeros((N, *y0.shape))
+    else:
+        ls = np.zeros((N,1))
     ls[0] = y0
         
     for i in range(1,N):
         ls[i] = f(ls[i-1], i-1)
             
     return ls
+
 
 
 def explicit_euler(rhs, y0, T, N):
@@ -21,23 +29,26 @@ def explicit_euler(rhs, y0, T, N):
 
 def implicit_euler(rhs, y0, T, N):
     h = T/(N-1)
-    return _solve_ode(lambda y_previous, i: fsolve(lambda y: y-y_previous-h*rhs(y, (i+1)*h), y_previous), y0, N)        #TODO Check
+    return _solve_ode(lambda y_previous, i: fsolve(lambda y: y-y_previous-h*rhs(y, (i+1)*h), y_previous+h*rhs(y_previous, i*h)), y0, N)        #TODO Check
 
 
 def implicit_trapezoidal(rhs, y0, T, N):
-    pass
+    h = T/(N-1)
+    return _solve_ode(lambda y_previous, i: fsolve(lambda y: y-y_previous-h*rhs(0.5*(y_previous+y), (i+0.5)*h), y_previous+h*rhs(y_previous, i*h)), y0, N)
 
 
 def implicit_midpoint(rhs, y0, T, N):
-    pass
+    h = T/(N-1)
+    return _solve_ode(lambda y_previous, i: fsolve(lambda y: y-y_previous-h/2*(rhs(y_previous, i*h)+rhs(y, (i+1)*h)), y_previous+h*rhs(y_previous, i*h)), y0, N)
 
 def velocity_verlet(f, y0, v0, T, N):
-    if(type(y0)==np.ndarray):
+    if type(y0)==np.ndarray:
         y = np.zeros((N, *y0.shape))
         v = np.zeros((N,*v0.shape))
     else:
         y = np.zeros((N, 1))
         v = np.zeros((N,1))
+        
     h = T/(N-1)
     h2 = (h**2)/2.0
     y[0] = y0
@@ -58,6 +69,11 @@ def f(x, t):
 def g(x, t):
     return -w**2*np.sin(x)
 
+def energy(x):
+    return (1-np.cos(x[:,0]))*m*gr*l + 0.5*m*(x[:,1]**2)
+
 if __name__=="__main__":
-    plt.plot(velocity_verlet(g, np.array([0.5]), np.array([0]), 3, 20000)[0])
+    result = implicit_trapezoidal(f, np.array([0.5, 0]), 16, 2000)
+    plt.plot(energy(result))
+    plt.plot(result[:,0])
     plt.show()
