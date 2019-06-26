1d7d6f31d3874f45d90ace7988ce3c7019789763
Markus Krimmel <montcyril@gmail.com>
2019-06-26 13:58:46 +0200

    QR-Decomposition non-square

--- linalg/decompose.py
+++ linalg/decompose.py
@@ -5,19 +5,21 @@ def factor_QR(A):
     if A.size == 1:                     #Base case
         return A/np.linalg.norm(A), np.linalg.norm(A)
 
-
-    x_bar = np.zeros((A.shape[1]))
+    if A.size == 0:
+        return np.eye(A.shape[0]), A
+    
+    x_bar = np.zeros((A.shape[0]))
     x_bar[0] = np.linalg.norm(A[:,0])
 
     u = A[:,0]-x_bar
 
     u = u/np.linalg.norm(u)
     
-    mirror = (np.eye(A.shape[1])-2*np.outer(u, u))
+    mirror = (np.eye(A.shape[0])-2*np.outer(u, u))
 
     B = np.matmul(mirror, A)
 
-    Q_next = np.eye((A.shape[1]))
+    Q_next = np.eye((A.shape[0]))
     Q_next[1:, 1:], B[1:, 1:] = factor_QR(B[1:, 1:])
 
     return np.matmul(mirror.T, Q_next), B
@@ -27,6 +29,9 @@ def rotation_QR(A):
     if A.size == 1:                     #Base case
         return A/np.linalg.norm(A), np.linalg.norm(A)
 
+    if A.size == 0:
+        return np.eye(A.shape[0]), A
+    
     B = A.copy()
 
     Q = np.eye(B.shape[0])
@@ -54,9 +59,8 @@ def rotation_QR(A):
 
 
 def gram_schmidt(A):
-    Q = np.zeros_like(A, np.complex_)
+    Q = np.zeros((A.shape[0], A.shape[0]), np.complex_)
     R = np.zeros_like(A, np.complex_)
-
     Q[:,0] = A[:, 0]
     norm = np.linalg.norm(Q[:, 0])
     Q[:, 0] = Q[:, 0]/norm
@@ -78,7 +82,7 @@ def gram_schmidt(A):
 
 def modified_gram_schmidt(A):
     R = np.zeros_like(A, np.complex_)
-    Q = np.zeros_like(A, np.complex_)
+    Q = np.zeros((A.shape[0], A.shape[0]), np.complex_)
 
     V = A.astype(np.complex_)
     
@@ -95,8 +99,9 @@ def modified_gram_schmidt(A):
 
 
 if __name__=="__main__":
-    Q, R = factor_QR(np.array([[1.0,2.0, -4, 1], [3.0, 4.0,234.4, -3], [2,1,2,0.0004], [0,0,2,0]]))
-    print(Q)
+    #Q, R = factor_QR(np.array([[1.0,2.0, -4, 1], [3.0, 4.0,234.4, -3], [2,1,2,0.0004], [0,0,2,0]]))
+    Q, R = factor_QR(np.array([[1, -2], [1, 0], [1, 1], [1, 3]]).T)
+    print(np.matmul(Q, Q.T))
     print()
     print(R)
     print()
